3a0480c8c5a0c3f30eaef82a2913bf26d5256239
Dyna-Rise <dynarise.headquarters@gmail.com>
2025-06-16 14:21:05 +0900

    Doorオブジェクトの完成

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Door : MonoBehaviour
@@ -8,6 +9,15 @@ public class Door : MonoBehaviour
     public Sprite goldDoorImage; //金の扉の絵
 
     public bool isEkey; //ボタンを押して反応するイベント
+    bool inDoorArea; //ドアの領域
+    bool isDelete; //削除すべきかどうか
+
+    //トークキャンバスのオブジェクト達を認識できるようにする
+    GameObject messageCanvas;
+    GameObject messagePanel;
+    TextMeshProUGUI messageText;
+
+    bool talking; //会話発生中かどうか
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +27,72 @@ public class Door : MonoBehaviour
             //見た目を変える
             GetComponent<SpriteRenderer>().sprite = goldDoorImage;
         }
+
+        //TalkCanvasを見つける
+        messageCanvas = GameObject.FindGameObjectWithTag("Talk");
+        //TalkCanvasの子どもから"TalkPanel"というオブジェクトを探す
+        messagePanel = messageCanvas.transform.Find("TalkPanel").gameObject;
+        messageText = messagePanel.transform.Find("TalkText").gameObject.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //キーイベントのフラグがONでDoorの近くにいる場合
+            if(isEkey && inDoorArea　&& !talking)
+            {
+                //金の扉
+                if (isGoldDoor && GameController.hasGoldKey > 0)
+                {
+                    GameController.hasGoldKey--; //鍵を消費
+                    isDelete = true; //削除フラグを立てる
+                    messagePanel.SetActive(true); //メッセージパネル表示
+                    messageText.text = "金の鍵を使った！";
+                    talking = true; //会話モードON
+                    GameController.gameState = "talk";
+                    Time.timeScale = 0; //ゲーム進行を止める
+                }
+                //銀の扉
+                else if(!isGoldDoor && GameController.hasSilverKey > 0)
+                {
+                    GameController.hasSilverKey--; //鍵を消費
+                    isDelete = true; //削除フラグを立てる
+                    messagePanel.SetActive(true); //メッセージパネル表示
+                    messageText.text = "銀の鍵を使った！";
+                    talking = true; //会話モードON
+                    GameController.gameState = "talk";
+                    Time.timeScale = 0; //ゲーム進行を止める
+                }
+                //いずれにしても鍵がない
+                else
+                {
+                    messagePanel.SetActive(true); //メッセージパネル表示
+                    messageText.text = "鍵がかかっている";
+                    talking = true; //会話モードON
+                    GameController.gameState = "talk";
+                    Time.timeScale = 0; //ゲーム進行を止める
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //すでにトークウィンドウが表示されているならばスペースキーでウィンドウを閉じる
+            if (talking && GameController.gameState == "talk")
+            {
+                messagePanel.SetActive(false); //パネルを閉じる
+                talking = false; //会話中フラグをOFF
+                GameController.gameState = "playing"; //ゲームステータスを元に戻す
+                Time.timeScale = 1f; //時の流れを元に戻す
+
+                if (isDelete)
+                {
+                    Destroy(gameObject); //鍵を開けた場合のみ削除
+                }
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -45,4 +115,23 @@ public class Door : MonoBehaviour
             }
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //キーイベントあり、相手がプレイヤーであったら
+        if(isEkey && collision.gameObject.CompareTag("Player"))
+        {
+            inDoorArea = true; //ドアの領域内に侵入
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //キーイベントあり、相手がプレイヤーであったら
+        if (isEkey && collision.gameObject.CompareTag("Player"))
+        {
+            inDoorArea = false; //ドアの領域から抜ける
+        }
+    }
+
 }
